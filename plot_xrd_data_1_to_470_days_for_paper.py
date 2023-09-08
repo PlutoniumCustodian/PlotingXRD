8ccf48d1568d1bbadd1905c21537de664ea86b88
Titus Reed <Titus@psu.edu>
2023-09-08 11:08:07 -0400

    backing up files

--- plot_xrd_data_1_to_470_days_for_paper.py
+++ plot_xrd_data_1_to_470_days_for_paper.py
@@ -112,7 +112,7 @@ Al_corner.append(my_data_extract(3))
 Centroid.append(my_data_extract(4))
 Mg_axial.append(my_data_extract(1))
 Mg_corner.append(my_data_extract(2))
-
+P_free.append(my_data_extract(5))
 #%% print maximum value for each scan
 def maxcount(xrd_data):
     for n in range(len(xrd_data)):
@@ -138,10 +138,10 @@ maxcount(Centroid)
 
 #%% Settings for batch of graphs
 #Values for setting that are used multple places
-off_set = 8500 #used to shift graphs up or down
-lnthikness= .75
+off_set = 16000 #used to shift graphs up or down
+lnthikness= 1
 xlimits = [ 5, 65]
-ylimits = [-300, 2.8e4]
+ylimits = [-500, 6.2e4]
 legspot = 'upper right' # Determines where legend is placed
 
 font = FontProperties()
@@ -178,7 +178,7 @@ def xrd_quad_plot(xrd_data, plot_names, ColorPalet, svg_file_name, plt_title, of
     ax.tick_params(axis='y',length=0)
     # ax.axes.xaxis.set_ticklabels([])
     ax.spines[['right', 'top']].set_visible(False)
-    plt.title(plt_title, loc='right', fontsize=9)
+    plt.title(plt_title, loc='right', fontsize=10)
     
     #Revers order of legend lables
     handles, labels = ax.get_legend_handles_labels()
@@ -186,40 +186,40 @@ def xrd_quad_plot(xrd_data, plot_names, ColorPalet, svg_file_name, plt_title, of
 
     svg_name_path = 'Plots/1_to_740days/XRD_Paper/' + svg_file_name + '.svg'
     # Uncomment next line to save the figure.
-    fig.savefig(svg_name_path, transparent=False, bbox_inches="tight")
+    # fig.savefig(svg_name_path, transparent=False, bbox_inches="tight")
     return fig
 
-#%% Al-axial plot
+# #%% Al-axial plot
 
-# order of inputs for xrd_quad_plot
-# xrd_data, plot_names, ColorPalet, svg_file_name, plt_title
-xrd_quad_plot(Al_axial, Data_lable , ColorB,\
-              'Al_axial_1to470day_forpaper2','Moderate-Aluminum', off_set)
+# # order of inputs for xrd_quad_plot
+# # xrd_data, plot_names, ColorPalet, svg_file_name, plt_title
+# xrd_quad_plot(Al_axial, Data_lable , ColorB,\
+#               'Al_axial_1to470day_forpaper2','Moderate-Aluminum', off_set)
 
-#%% Al-corner plot
+# #%% Al-corner plot
 
-xrd_quad_plot(Al_corner , Data_lable , ColorB,\
-              'Al_corner_1to470day_forpaper2', 'High-Aluminum', 13500)
+# xrd_quad_plot(Al_corner , Data_lable , ColorB,\
+#               'Al_corner_1to470day_forpaper2', 'High-Aluminum', 13500)
 
-#%% Mg-Axial
+# #%% Mg-Axial
 
-xrd_quad_plot(Mg_axial , Data_lable , ColorG, 'Mg_Axial_1to470day_forpaper2',\
-              'Moderate-Magnesium', off_set)
+# xrd_quad_plot(Mg_axial , Data_lable , ColorG, 'Mg_Axial_1to470day_forpaper2',\
+#               'Moderate-Magnesium', off_set)
 
-#%% Mg-Corner plot
+# #%% Mg-Corner plot
 
-xrd_quad_plot(Mg_corner , Data_lable , ColorG, 'Mg_corner_1to470day_forpape2r',\
-              'High-Magnesium', off_set)
+# xrd_quad_plot(Mg_corner , Data_lable , ColorG, 'Mg_corner_1to470day_forpape2r',\
+#               'High-Magnesium', off_set)
 
-#%% PO4-Free plot
+# #%% PO4-Free plot
 
-xrd_quad_plot(P_free , Data_lable , ColorO, 'PO4_free_1and28_day_forpaper2',\
-              'No-Phosphate', 7000)
+# xrd_quad_plot(P_free , Data_lable , ColorO, 'PO4_free_1and28_day_forpaper2',\
+#               'No-Phosphate', 7000)
 
-#%% Centroid plot
+# #%% Centroid plot
 
-xrd_quad_plot(Centroid , Data_lable , ColorK, 'Centroid_1to_510day_forpaper2',\
-              'High-Phosphate', 7000)
+# xrd_quad_plot(Centroid , Data_lable , ColorK, 'Centroid_1to_510day_forpaper2',\
+#               'High-Phosphate', 7000)
 
 #%% multy plot function
 
@@ -232,10 +232,10 @@ def plot_loop(ax, xrd_data, plot_names, ColorPalet, off_set):
      
      # ax.set_xlabel("2θ (degrees)", fontsize=9)
      # ax.set_ylabel("Intensity", fontsize=9)
-     ax.tick_params(axis='x', labelsize=8)
-     ax.tick_params(axis='y', labelsize=8)
+     ax.tick_params(axis='x', labelsize=12)
+     ax.tick_params(axis='y', labelsize=12)
      ax.set_xlim(xlimits)
-     # ax.set_ylim(ylimits)
+     ax.set_ylim(ylimits)
      ax.xaxis.set_minor_locator(MultipleLocator(2.5))
      ax.yaxis.set_ticklabels([])
      ax.tick_params(axis='y',length=0)
@@ -247,7 +247,7 @@ def plot_loop(ax, xrd_data, plot_names, ColorPalet, off_set):
      # ax.legend(handles[::-1], labels[::-1])
      
 def subplot_title(ax, text, set_color):
-    ax.set_title(text, loc='right', fontsize=8, fontstyle='normal', color=set_color)
+    ax.set_title(text, loc='right', fontsize=9, fontstyle='normal', color=set_color)
 
     
 #%% Grid of plots
@@ -263,7 +263,7 @@ ax0.axes.xaxis.set_ticklabels([])
 
 ax1 = fig.add_subplot(spec[0, 1])
 n=0
-H_Al_off = 14000
+H_Al_off = off_set #1400
 n1 = .6
 n2 = .55
 ax1.plot(Al_corner[n][0,:], Al_corner[n][1,:] + n*H_Al_off, 
@@ -281,10 +281,10 @@ ax1.plot(Al_corner[n][0,:], Al_corner[n][1,:] + ((n - 1) + n1)*H_Al_off,
 
 # ax.set_xlabel("2θ (degrees)", fontsize=9)
 # ax.set_ylabel("Intensity", fontsize=9)
-ax1.tick_params(axis='x', labelsize=8)
-ax1.tick_params(axis='y', labelsize=8)
+ax1.tick_params(axis='x', labelsize=10)
+ax1.tick_params(axis='y', labelsize=10)
 ax1.set_xlim(xlimits)
-# ax.set_ylim(ylimits)
+ax1.set_ylim(ylimits)
 ax1.xaxis.set_minor_locator(MultipleLocator(2.5))
 ax1.yaxis.set_ticklabels([])
 ax1.tick_params(axis='y',length=0)
@@ -297,19 +297,19 @@ ax2 = fig.add_subplot(spec[1, 0])
 plot_loop(ax2, Mg_axial, Data_lable , ColorG, off_set)
 subplot_title(ax2,'Moderate-Magnesium',ColorG[3])
 ax2.axes.xaxis.set_ticklabels([])
-ax2.set_ylabel("Intensity", fontsize=9)
+ax2.set_ylabel("Intensity", fontsize=14)
 
 ax3 = fig.add_subplot(spec[1, 1])
-plot_loop(ax3, Mg_corner, Data_lable , ColorG, 11000)
+plot_loop(ax3, Mg_corner, Data_lable , ColorG, off_set) #11000
 subplot_title(ax3,'High-Magnesium',ColorG[3])
 ax3.axes.xaxis.set_ticklabels([])
 
 ax4 = fig.add_subplot(spec[2, 0])
-plot_loop(ax4, P_free, Data_lable , ColorO, 7000)
+plot_loop(ax4, P_free, Data_lable , ColorO, off_set) #7000
 subplot_title(ax4,'No-Phosphate',ColorO[3])
 
 ax5 = fig.add_subplot(spec[2, 1])
-plot_loop(ax5, Centroid, Data_lable , ColorK, 7000)
+plot_loop(ax5, Centroid, Data_lable , ColorK, off_set) #7000
 subplot_title(ax5,'High-Phosphate',ColorK[3])
 
 
@@ -317,21 +317,21 @@ subplot_title(ax5,'High-Phosphate',ColorK[3])
 fig.add_subplot(111, frameon=False)
 # hide tick and tick label of the big axis
 plt.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
-plt.xlabel("2θ (degrees)", fontsize=9)
+plt.xlabel("2θ (degrees)", fontsize=14)
 # plt.ylabel("Intensity")
 
-fig.savefig("Plots/1_to_740days/XRD_Paper/time_series_V1.svg", transparent=False, bbox_inches="tight")
+fig.savefig("Plots/1_to_740days/XRD_Paper/time_series_V5.svg", transparent=False, bbox_inches="tight")
 
 # plt.close('all')
 
 #%%
 
-fig, axes = plt.subplots(4, 2, sharex=True, sharey=True, figsize=(6,15))
-# add a big axis, hide frame
-fig.add_subplot(111, frameon=False)
-# hide tick and tick label of the big axis
-plt.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
-plt.xlabel("common X")
-plt.ylabel("common Y")
+# fig, axes = plt.subplots(4, 2, sharex=True, sharey=True, figsize=(6,15))
+# # add a big axis, hide frame
+# fig.add_subplot(111, frameon=False)
+# # hide tick and tick label of the big axis
+# plt.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
+# plt.xlabel("common X")
+# plt.ylabel("common Y")
 
 
--- plot_xrd_from_xy_file_water_washed_5_day.py
+++ plot_xrd_from_xy_file_water_washed_5_day.py
@@ -21,7 +21,7 @@ print(pd.DataFrame(f_name))
 XRDdata = []
 
 #Name your data
-XRD_label = [ 'H-Mg','M-Mg', 'H-Al', 'M-Al', 'H-P', 'M-P', 'N-P' ]
+XRD_label = ['N-P', 'L-P', 'H-P', 'M-Mg', 'H-Mg','M-Al', 'H-Al']
 
 #%% Import data
 #This section will read only the diffraction data of the  xy  files in the folder
@@ -58,8 +58,8 @@ for n in f_howmany:
     
 #%% Settings for batch of graphs
 #Values for setting that are used multple places
-off_set = 1500 #used to shift graphs up or down
-lnthikness= 0.5
+off_set = 4500 #used to shift graphs up or down
+lnthikness= 1
 xlimits = [ 5, 65]
 ylimits = [.25, 3]
 legspot = 'upper right' # Determines where legend is placed
@@ -68,21 +68,23 @@ font = FontProperties()
 font.set_family('sans-serf')
 font.set_name('Arial')
 font.set_size(9)
+ColorPalet_1 = ['#c76f0a', '#626161', '#000000', '#58a265','#0e7723', '#5560a2', '#0e0e77']
+
 
 #%% Ploting Function
 test= 'Test_plot'
-ColorPalet_1 = ['#000000', '#73005d', '#b20046', '#d80c1d']
-def xrd_quad_plot(xrd_data, plot_names, ColorPalet, svg_file_name, plt_title):
-    num_of_scans = range(len(xrd_data))
-    fig, ax = plt.subplots(figsize=(8,5)) #size is in inches    
-    for n in num_of_scans:
-        ax.plot(xrd_data[n][0,:], xrd_data[n][1,:] + n*off_set, 
-        linewidth=lnthikness, label=plot_names[n]) #color=ColorPalet[n]
 
-    ax.set_xlabel("2θ (degrees)", fontsize=9)
-    ax.set_ylabel("Intensity", fontsize=9)
-    ax.tick_params(axis='x', labelsize=8)
-    ax.tick_params(axis='y', labelsize=8)
+def xrd_quad_plot(xrd_data, plot_names, ColorPalet, svg_file_name, plt_title, offset):
+    num_of_scans = range(len(xrd_data))
+ 
+    for n in num_of_scans:
+        ax.plot(xrd_data[n][0,:], xrd_data[n][1,:] + n*offset, 
+        linewidth=lnthikness, label=plot_names[n],  color=ColorPalet[n]) #color=ColorPalet[n]
+
+    ax.set_xlabel("2θ (degrees)", fontsize=14)
+    ax.set_ylabel("Intensity", fontsize=14)
+    ax.tick_params(axis='x', labelsize=10)
+    ax.tick_params(axis='y', labelsize=10)
     ax.set_xlim(xlimits)
     # ax.set_ylim(ylimits)
     ax.xaxis.set_minor_locator(MultipleLocator(2.5))
@@ -92,8 +94,8 @@ def xrd_quad_plot(xrd_data, plot_names, ColorPalet, svg_file_name, plt_title):
     ax.spines[['right', 'top']].set_visible(False)
     
     # #Revers order of legend lables
-    handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles[::-1], labels[::-1],loc='upper right')
+    # handles, labels = ax.get_legend_handles_labels()
+    # ax.legend(handles[::-1], labels[::-1],loc='upper right')
 
     svg_name_path = 'Plots/Washed_5_day_old/' + svg_file_name + '.svg'
     # Uncomment this line to save the figure.
@@ -101,69 +103,76 @@ def xrd_quad_plot(xrd_data, plot_names, ColorPalet, svg_file_name, plt_title):
     return fig
 
 #%% Plot
+fig, ax = plt.subplots(figsize=(3.95,5.8)) #size is in inches
+xrd_quad_plot(XRDdata, XRD_label, ColorPalet_1, 'All_5day_washed_stacked', '',3500 )
 
-xrd_quad_plot(XRDdata, XRD_label, ColorPalet_1, 'All_5day_washed_stacked', '')
+#%% Plot
+
+fig, ax = plt.subplots(figsize=(3.3,5)) #size is in inches  
+xrd_quad_plot(XRDdata, XRD_label, ColorPalet_1, 'All_5day_washed_003_peak_V2', '', 0)
+ax.set_xlim([ 7.5, 15])
+ax.set_ylim([0, 3500])
 
 #%% plot a few plot (Centroid to no phosphate)
-n=4
-fig, ax = plt.subplots(figsize=(8,5)) #size is in inches
-ax.plot(XRDdata[n][0,:], XRDdata[n][1,:], 
-    linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
+# n=4
+# fig, ax = plt.subplots(figsize=(8,5)) #size is in inches
+# ax.plot(XRDdata[n][0,:], XRDdata[n][1,:], 
+#     linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
 
-n=5
-ax.plot(XRDdata[n][0,:], XRDdata[n][1,:]+ 1*off_set, 
-    linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
+# n=5
+# ax.plot(XRDdata[n][0,:], XRDdata[n][1,:]+ 1*off_set, 
+#     linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
 
-n=6
-ax.plot(XRDdata[n][0,:], XRDdata[n][1,:]+ 2*off_set, 
-    linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
+# n=6
+# ax.plot(XRDdata[n][0,:], XRDdata[n][1,:]+ 2*off_set, 
+#     linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
 
-ax.set_xlabel("2θ (degrees)", fontsize=9)
-ax.set_ylabel("Intensity", fontsize=9)
-ax.tick_params(axis='x', labelsize=8)
-ax.tick_params(axis='y', labelsize=8)
-ax.set_xlim(xlimits)
-# ax.set_ylim(ylimits)
-ax.xaxis.set_minor_locator(MultipleLocator(2.5))
-ax.yaxis.set_ticklabels([])
-ax.tick_params(axis='y',length=0)
-# plt.title(plt_title)
-ax.spines[['right', 'top']].set_visible(False)
+# ax.set_xlabel("2θ (degrees)", fontsize=9)
+# ax.set_ylabel("Intensity", fontsize=9)
+# ax.tick_params(axis='x', labelsize=8)
+# ax.tick_params(axis='y', labelsize=8)
+# ax.set_xlim(xlimits)
+# # ax.set_ylim(ylimits)
+# ax.xaxis.set_minor_locator(MultipleLocator(2.5))
+# ax.yaxis.set_ticklabels([])
+# ax.tick_params(axis='y',length=0)
+# # plt.title(plt_title)
+# ax.spines[['right', 'top']].set_visible(False)
 
-# #Revers order of legend lables
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles[::-1], labels[::-1],loc='upper right')
+# # #Revers order of legend lables
+# handles, labels = ax.get_legend_handles_labels()
+# ax.legend(handles[::-1], labels[::-1],loc='upper right')
 
-svg_name_path = 'Plots/Washed_5_day_old/' + 'Centroid_to_PO4Free_5day_washed_offset' + '.svg'
-# Uncomment this line to save the figure.
-fig.savefig(svg_name_path, transparent=False, bbox_inches="tight")
-
-#%% plot a few plot (high Al ones)
-n=3
-fig, ax = plt.subplots(figsize=(8,5)) #size is in inches
-ax.plot(XRDdata[n][0,:], XRDdata[n][1,:], 
-    linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
-
-n=2
-ax.plot(XRDdata[n][0,:], XRDdata[n][1,:]+ 1*off_set, 
-    linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
-
-ax.set_xlabel("2θ (degrees)", fontsize=9)
-ax.set_ylabel("Intensity", fontsize=9)
-ax.tick_params(axis='x', labelsize=8)
-ax.tick_params(axis='y', labelsize=8)
-ax.set_xlim(xlimits)
-# ax.set_ylim(ylimits)
-ax.xaxis.set_minor_locator(MultipleLocator(2.5))
-ax.yaxis.set_ticklabels([])
-ax.tick_params(axis='y',length=0)
-# plt.title(plt_title)
-ax.spines[['right', 'top']].set_visible(False)
-
-# #Revers order of legend lables
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles[::-1], labels[::-1],loc='upper right')
-
-svg_name_path = 'Plots/Washed_5_day_old/' + 'High-Al_5day_washed' + '.svg'
-# Uncomment this line to save the figure.
+# svg_name_path = 'Plots/Washed_5_day_old/' + 'Centroid_to_PO4Free_5day_washed_offset' + '.svg'
+# # Uncomment this line to save the figure.
 # fig.savefig(svg_name_path, transparent=False, bbox_inches="tight")
+
+# #%% plot a few plot (high Al ones)
+# n=3
+# fig, ax = plt.subplots(figsize=(8,5)) #size is in inches
+# ax.plot(XRDdata[n][0,:], XRDdata[n][1,:], 
+#     linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
+
+# n=2
+# ax.plot(XRDdata[n][0,:], XRDdata[n][1,:]+ 1*off_set, 
+#     linewidth=lnthikness, label=XRD_label[n]) #color=ColorPalet[n], , XRD_label[n]
+
+# ax.set_xlabel("2θ (degrees)", fontsize=9)
+# ax.set_ylabel("Intensity", fontsize=9)
+# ax.tick_params(axis='x', labelsize=8)
+# ax.tick_params(axis='y', labelsize=8)
+# ax.set_xlim(xlimits)
+# # ax.set_ylim(ylimits)
+# ax.xaxis.set_minor_locator(MultipleLocator(2.5))
+# ax.yaxis.set_ticklabels([])
+# ax.tick_params(axis='y',length=0)
+# # plt.title(plt_title)
+# ax.spines[['right', 'top']].set_visible(False)
+
+# # #Revers order of legend lables
+# handles, labels = ax.get_legend_handles_labels()
+# ax.legend(handles[::-1], labels[::-1],loc='upper right')
+
+# svg_name_path = 'Plots/Washed_5_day_old/' + 'High-Al_5day_washed' + '.svg'
+# # Uncomment this line to save the figure.
+# # fig.savefig(svg_name_path, transparent=False, bbox_inches="tight")
